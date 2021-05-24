12f992b4924f6f0ee5d5e1eb26ccc2ec64fa47b1
Marek <grigori93@gmail.com>
2021-05-24 14:16:15 +0200

    commit -m "projekt"

--- src/components/QuestionsScreen/QuestionScreen.js
+++ src/components/QuestionsScreen/QuestionScreen.js
@@ -29,7 +29,7 @@ export const QuestionsScreen = ({id}) => {
                 // increase score
                 setScore(score + 1);
             }
-        },300)
+        },310)
 
     }
     const handleRetryButton = ()=> {
